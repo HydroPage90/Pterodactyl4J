cfa0f5db8d26b075bd881278479c7e9ddd0e1361
HydroPage90 <alejolaverde0927@gmail.com>
2020-12-04 14:24:00 -0600

    AbstractNodeAction changes

--- src/main/java/com/mattmalec/pterodactyl4j/application/entities/impl/EditNodeImpl.java
+++ src/main/java/com/mattmalec/pterodactyl4j/application/entities/impl/EditNodeImpl.java
@@ -7,78 +7,60 @@ import com.mattmalec.pterodactyl4j.entities.PteroAction;
 import com.mattmalec.pterodactyl4j.requests.Route;
 import org.json.JSONObject;
 
+import static java.lang.Long.parseLong;
+
 public class EditNodeImpl extends AbstractNodeAction {
 
-	private final Node node;
+    private final Node node;
 
-	EditNodeImpl(PteroApplicationImpl impl, Node node) {
-		super(impl);
-		this.node = node;
-	}
+    EditNodeImpl(PteroApplicationImpl impl, Node node) {
+        super(impl);
+        this.node = node;
+    }
 
-	@Override
-	public PteroAction<Node> build() {
-		return PteroActionImpl.onExecute(() -> {
-			JSONObject json = new JSONObject();
-			if (name == null)
-				json.put("name", node.getName());
-			else
-				json.put("name", name);
-			if (location == null)
-				json.put("location_id", node.getLocation().retrieve().execute().getId());
-			else
-				json.put("location_id", location.getId());
-			if (isPublic == null)
-					json.put("public", node.isPublic() ? "1" : "0");
-				else
-					json.put("public", isPublic ? "1" : "0");
-				if(fqdn == null)
-					json.put("fqdn", node.getFQDN());
-				else
-					json.put("fqdn", fqdn);
-				if(secure == null)
-					json.put("scheme", node.getScheme());
-				else
-					json.put("scheme", secure ? "https" : "http");
-				if(isBehindProxy == null)
-					json.put("behind_proxy", node.isBehindProxy() ? "1" : "0");
-				else
-					json.put("behind_proxy", isBehindProxy ? "1" : "0");
-				if(daemonBase == null)
-					json.put("daemon_base", node.getDaemonBase());
-				else
-					json.put("daemon_base", daemonBase);
-				if(memory == null)
-					json.put("memory", node.getMemoryLong());
-				else
-					json.put("memory", Long.parseLong(memory));
-				if(memoryOverallocate == null)
-					json.put("memory_overallocate", node.getMemoryOverallocateLong());
-				else
-					json.put("memory_overallocate", Long.parseLong(memoryOverallocate));
-				if(diskSpace == null)
-					json.put("disk", node.getDiskLong());
-				else
-					json.put("disk", Long.parseLong(diskSpace));
-				if(diskSpaceOverallocate == null)
-					json.put("disk_overallocate", node.getDiskOverallocateLong());
-				else
-					json.put("disk_overallocate", Long.parseLong(diskSpaceOverallocate));
-				if(daemonListenPort == null)
-					json.put("daemon_listen", node.getDaemonListenPort());
-				else
-					json.put("daemon_listen", daemonListenPort);
-				if(daemonSFTPPort == null)
-					json.put("daemon_sftp", node.getDaemonSFTPPort());
-				else
-					json.put("daemon_sftp", daemonSFTPPort);
-			if (throttle == null)
-				json.put("throttle", new JSONObject().put("enabled", false));
-			else
-				json.put("throttle", new JSONObject().put("enabled", throttle));
-			Route.CompiledRoute route = Route.Nodes.EDIT_NODE.compile(node.getId()).withJSONdata(json);
-			JSONObject jsonObject = requester.request(route).toJSONObject();
-			return new NodeImpl(jsonObject, impl);
-		});
-	}
+    @Override
+    public PteroAction<Node> build() {
+        return PteroActionImpl.onExecute(() -> {
+            JSONObject json = new JSONObject();
+
+            String nameToPut = (name == null) ? node.getName() : name;
+            String isPublic = boolBit((this.isPublic == null) ? node.isPublic() : this.isPublic);
+            long memoryToPut = (memory == null) ? node.getMemoryLong() : parseLong(memory);
+            String fqdnToPut = (fqdn == null) ? node.getFQDN() : fqdn;
+            String schemeToPut = (secure == null) ? node.getScheme() : (secure ? "https" : "http");
+            long diskSpaceToPut = (diskSpace == null) ? node.getIdLong() : parseLong(diskSpace);
+            String locationToPut = (location == null) ? node.getLocation().retrieve().execute().getId() : location.getId();
+            String isBehindProxy = boolBit((this.isBehindProxy == null) ? node.isBehindProxy() : this.isBehindProxy);
+            boolean throttleToPut = (throttle != null) && throttle;
+            String daemonBaseToPut = (daemonBase == null) ? node.getDaemonBase() : daemonBase;
+            long memoryOverallocate = (this.memoryOverallocate == null) ? node.getMemoryOverallocateLong() : parseLong(this.memoryOverallocate);
+            long diskSpaceOverallocate = (this.diskSpaceOverallocate == null) ? node.getDiskOverallocateLong() : parseLong(this.diskSpaceOverallocate);
+            String daemonSFTPPortToPut = (daemonSFTPPort == null) ? node.getDaemonSFTPPort() : daemonSFTPPort;
+            String deamonListenPortToPut = (daemonListenPort == null) ? node.getDaemonListenPort() : daemonListenPort;
+
+            json.put("name", nameToPut);
+            json.put("fqdn", fqdnToPut);
+            json.put("public", isPublic);
+            json.put("scheme", schemeToPut);
+            json.put("memory", memoryToPut);
+            json.put("disk", diskSpaceToPut);
+            json.put("location_id", locationToPut);
+            json.put("behind_proxy", isBehindProxy);
+            json.put("daemon_base", daemonBaseToPut);
+            json.put("daemon_sftp", daemonSFTPPortToPut);
+            json.put("daemon_listen", deamonListenPortToPut);
+            json.put("memory_overallocate", memoryOverallocate);
+            json.put("disk_overallocate", diskSpaceOverallocate);
+            json.put("throttle", new JSONObject().put("enabled", throttleToPut));
+
+
+            Route.CompiledRoute route = Route.Nodes.EDIT_NODE.compile(node.getId()).withJSONdata(json);
+            JSONObject jsonObject = requester.request(route).toJSONObject();
+            return new NodeImpl(jsonObject, impl);
+        });
+    }
+
+    private static String boolBit(boolean in) {
+        return in ? "1" : "0";
+    }
 }
